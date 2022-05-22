e6ca7622ee458f390e1f08d1a024ae6825e5461d
Clembs <clembs@clembs.xyz>
2022-05-22 14:00:10 +0200

    fixed

--- src/routes/blog/[post].svelte
+++ src/routes/blog/[post].svelte
@@ -1,38 +1,15 @@
-<!-- <script context="module" lang="ts">
-	export const prerender = false;
-
-	export const load: Load = async ({ params }) => {
-		const post = await getPost(params.post);
-		console.log(post.data);
-
-		if (post !== null) {
-			return {
-				props: {
-					post: post.code,
-					metadata: post.data.fm
-				}
-			};
-		}
-	};
-</script> -->
 <script lang="ts">
-	// import { getPost } from '$lib/blog';
-	// import type { Load } from '@sveltejs/kit';
 	import MetaTags from '$lib/components/MetaTags.svelte';
 	import BlogHeader from '$lib/components/BlogHeader.svelte';
-	// import '../../styles/markdown.scss';
 
 	export let metadata: any;
 	export let post: string;
 </script>
 
-<MetaTags title={metadata.title} description={metadata.description} />
+<MetaTags {...metadata} />
 
 <BlogHeader {metadata} />
 
 <section itemprop="articleBody">
 	{@html post}
-	<!-- {@html replace(marked(post), ({ emoji, key }) => {
-      return `<img class="emoji" src="https://clembs.xyz/emojis/${key}.png" alt="${emoji}">`;
-    })} -->
 </section>
--- src/routes/blog/[post].ts
+++ src/routes/blog/[post].ts
@@ -2,9 +2,9 @@ import type { RequestHandler } from '@sveltejs/kit';
 import { readFileSync } from 'fs';
 import { compile } from 'mdsvex';
 
-export const get: RequestHandler = async ({ params }) => {
+export const get: RequestHandler = async ({ params, url }) => {
 	try {
-		const raw = readFileSync(`./src/lib/blog/${params.post}.md`, 'utf8');
+		const raw = await (await fetch(`${url.href.replace('blog', 'markdown')}.mdx`)).text();
 		const { code: post, data: meta } = await compile(raw);
 
 		return {
@@ -14,11 +14,10 @@ export const get: RequestHandler = async ({ params }) => {
 			}
 		};
 	} catch (e) {
+		console.error(e);
+
 		return {
-			status: 404,
-			body: {
-				error: e.message
-			}
+			status: 404
 		};
 	}
 };
