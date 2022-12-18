84cc297ef67bdf86d1ba18787d0467e1766ed59f
Clembs <clembs@clembs.com>
2022-12-19 00:36:32 +0100

    feat: more page

---
+++ src/routes/(marketing)/WebsiteLink/_links.ts
@@ -0,0 +1,69 @@
+import Scroll from '$lib/svg/Scroll.svelte';
+import Discord from '$lib/svg/brands/discord.svelte';
+import Twitter from '$lib/svg/brands/twitter.svelte';
+import Purplet from '$lib/svg/Purplet.svelte';
+import { BookOpen, ExternalLink, Github, Heart, Lock, UserPlus } from 'lucide-svelte';
+
+export interface PageLink {
+	icon: any;
+	href: string;
+	label: string;
+}
+
+export const links: PageLink[] = [
+	{
+		href: '/donate',
+		icon: Heart,
+		label: 'Donate'
+	},
+	{
+		href: '/invite',
+		icon: UserPlus,
+		label: 'Add CRBT to Discord'
+	},
+	{
+		href: '/topgg',
+		icon: ExternalLink,
+		label: 'Vote on Top.gg'
+	},
+	{
+		href: '/discord',
+		icon: Discord,
+		label: 'Join the CRBT Community'
+	},
+	{
+		href: '/docs',
+		icon: BookOpen,
+		label: 'Docs'
+	},
+	{
+		href: '/discordbots',
+		icon: ExternalLink,
+		label: 'Discord Bots page'
+	},
+	{
+		href: '/acknowledgments',
+		icon: Scroll,
+		label: 'Acknowledgements'
+	},
+	{
+		href: '/github',
+		icon: Github,
+		label: 'GitHub'
+	},
+	{
+		href: '/twitter',
+		icon: Twitter,
+		label: 'Twitter'
+	},
+	{
+		href: '/policy',
+		icon: Lock,
+		label: 'Privacy Policy'
+	},
+	{
+		href: 'https://purplet.js.org',
+		icon: Purplet,
+		label: 'Purplet, the Discord app framework by CRBT.'
+	}
+];
---
+++ src/routes/(marketing)/more/+page.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import Back from '$lib/components/Back.svelte';
+	import Links from '../WebsiteLink/Links.svelte';
+</script>
+
+<Back />
+
+<h1>More</h1>
+
+<Links />
+
+<style lang="scss">
+	h1 {
+		font-size: 3rem;
+		font-weight: 400;
+		padding: 20px;
+		margin-bottom: 0.5rem;
+	}
+</style>
