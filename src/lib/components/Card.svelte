ab5959f357d99a589effbd06ce4cb48c4360c1c2
Clembs <clembs@clembs.com>
2022-12-19 00:34:44 +0100

    feat: change card style

--- src/lib/components/Card.svelte
+++ src/lib/components/Card.svelte
@@ -6,7 +6,8 @@
 	div {
 		display: flex;
 		flex-direction: column;
-		background-color: var(--color-surface);
+		// background-color: var(--color-surface);
+		border: 1px solid var(--color-primary);
 		color: var(--color-on-surface);
 		border-radius: var(--border-radius-medium);
 		padding: 15px;
