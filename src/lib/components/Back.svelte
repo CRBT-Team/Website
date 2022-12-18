340bcc473becf1eddbcb1035f1e095f1705ab485
Clembs <clembs@clembs.com>
2022-12-19 00:30:41 +0100

    feat: back btn component

---
+++ src/lib/components/Back.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import { ArrowLeft } from 'lucide-svelte';
+
+	export let href = '/';
+</script>
+
+<div class="back-btn">
+	<a {href}>
+		<ArrowLeft />
+	</a>
+</div>
+
+<style lang="scss">
+	.back-btn {
+		padding: 1.5rem 1rem;
+		margin-bottom: 3.8rem;
+	}
+</style>
