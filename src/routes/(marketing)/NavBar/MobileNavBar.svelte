2a97410aaf8b2ff0e5e9a6043e46900b3a8a47f3
Clembs <clembs@clembs.com>
2022-12-23 22:25:47 +0100

    feat: add to discord FAB on the mobile navbar

--- src/routes/(marketing)/NavBar/MobileNavBar.svelte
+++ src/routes/(marketing)/NavBar/MobileNavBar.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import { page } from '$app/stores';
+	import AddToDiscord from '$lib/components/AddToDiscord.svelte';
 	import Modal from '$lib/components/Modal.svelte';
 	import TextField from '$lib/components/TextField.svelte';
 	import CRBT from '$lib/svg/crbt.svelte';
@@ -13,6 +14,7 @@
 	let showSearchModal = false;
 
 	page.subscribe(({ url: { pathname } }) => {
+		console.log(page);
 		if (pathname.endsWith('/')) selectTab('home');
 		if (pathname.endsWith('more')) selectTab('more');
 	});
@@ -20,10 +22,19 @@
 	function selectTab(tab: Tab) {
 		selectedTab = tab;
 	}
+
+	function toggleSearch() {
+		if (showSearchModal) {
+			document.body.classList.remove('prevent-scroll');
+		} else {
+			document.body.classList.add('prevent-scroll');
+		}
+		showSearchModal = !showSearchModal;
+	}
 </script>
 
 {#if showSearchModal}
-	<Modal title="Search" on:close={() => (showSearchModal = false)}>
+	<Modal title="Search" on:close={toggleSearch}>
 		<div class="middle">
 			<TextField on:input={(t) => (searchValue = t.detail)} type="text" />
 		</div>
@@ -33,47 +44,68 @@
 	</Modal>
 {/if}
 
-<nav class="navbar-mobile">
-	<a
-		href="/"
-		class="item"
-		class:selected={selectedTab === 'home'}
-		on:click={() => selectTab('home')}
-	>
-		<div class="background" />
-		<div class="icon">
-			<CRBT />
-		</div>
-		Home
-	</a>
-	<button class="item" on:click={() => (showSearchModal = true)}>
-		<div class="background" />
-		<div class="icon">
-			<Search />
-		</div>
-		Search
-	</button>
-	<a
-		href="/more"
-		class="item"
-		class:selected={selectedTab === 'more'}
-		on:click={() => selectTab('more')}
-	>
-		<div class="background" />
-		<div class="icon">
-			<Menu />
-		</div>
-		More
-	</a>
-</nav>
+<div class="navbar-mobile-wrapper">
+	<div class="fab">
+		<AddToDiscord />
+	</div>
+	<nav class="navbar-mobile">
+		<a
+			href="/"
+			class="item"
+			class:selected={selectedTab === 'home'}
+			on:click={() => selectTab('home')}
+		>
+			<div class="background" />
+			<div class="icon">
+				<CRBT />
+			</div>
+			Home
+		</a>
+		<button class="item" on:click={toggleSearch}>
+			<div class="background" />
+			<div class="icon">
+				<Search />
+			</div>
+			Search
+		</button>
+		<a
+			href="/more"
+			class="item"
+			class:selected={selectedTab === 'more'}
+			on:click={() => selectTab('more')}
+		>
+			<div class="background" />
+			<div class="icon">
+				<Menu />
+			</div>
+			More
+		</a>
+	</nav>
+</div>
 
 <style lang="scss">
+	.navbar-mobile-wrapper {
+		position: fixed;
+		flex-direction: column;
+		width: 100%;
+		bottom: 0;
+		left: 0;
+		z-index: 9;
+		display: flex;
+		align-items: flex-end;
+		gap: 10px;
+
+		.fab {
+			margin-right: 10px;
+		}
+	}
+
 	.navbar-mobile {
 		display: flex;
 		justify-content: space-around;
-		position: fixed;
-		bottom: 0;
-		left: 0;
+		// position: fixed;
+		// bottom: 0;
+		// left: 0;
 		z-index: 9;
 		width: 100%;
 		background-color: var(--color-surface);
@@ -133,7 +165,7 @@
 	}
 
 	@media (min-width: 800px) {
-		.navbar-mobile {
+		.navbar-mobile-wrapper {
 			display: none;
 		}
 	}
@@ -146,5 +178,6 @@
 	.bottom {
 		overflow-y: scroll;
 		width: 100%;
+		padding: 0px 20px;
 	}
 </style>
