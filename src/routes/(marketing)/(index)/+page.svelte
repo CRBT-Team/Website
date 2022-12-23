e6baabfc65cc135701a539a4d7d205dd1f56ecca
Clembs <clembs@clembs.com>
2022-12-23 02:06:37 +0100

    feat: big feature & first illustration

--- src/routes/(marketing)/(index)/+page.svelte
+++ src/routes/(marketing)/(index)/+page.svelte
@@ -1,20 +1,33 @@
 <script lang="ts">
 	export const prerender = true;
 
-	import Compare from './Compare.svelte';
 	import FeaturedServers from './FeaturedServers.svelte';
-	import Features from './Features.svelte';
+	import FeatureCarousel from './FeatureCarousel.svelte';
 	import Header from './Header.svelte';
+	import Feature from './Feature.svelte';
+	import MetaTags from '$lib/components/MetaTags.svelte';
 </script>
+
+<MetaTags />
 
 <Header />
 
 <div class="content">
 	<FeaturedServers />
 
-	<Features />
+	<!-- svelte-ignore a11y-missing-content -->
+	<a name="features" />
 
-	<Compare />
+	<Feature
+		title="Wave hi to your new Discord members"
+		description="Use your welcome message to convey concise information about your server's theme, events and guidelines. Customize it with CRBT's branding options to make your server stand out, or keep things simple."
+		imageUrl="/assets/welcome.svg"
+		position="left"
+	/>
+
+	<FeatureCarousel />
+
+	<!-- <Compare /> -->
 </div>
 
 <style lang="scss">
---
+++ src/routes/(marketing)/(index)/Feature.svelte
@@ -0,0 +1,53 @@
+<script lang="ts">
+	export let title: string;
+	export let description: string;
+	export let imageUrl: string;
+	export let position: 'left' | 'right';
+</script>
+
+<div class="feature {position}">
+	<img class="image" alt={title} src={imageUrl} />
+
+	<div class="text">
+		<h1 class="title">{title}</h1>
+		<p class="description">{description}</p>
+	</div>
+</div>
+
+<style lang="scss">
+	.feature {
+		display: flex;
+		gap: 50px;
+		max-width: 100rem;
+		align-items: center;
+
+		.image {
+			max-width: 100%;
+			min-width: 50%;
+		}
+
+		&.left {
+			flex-direction: row;
+		}
+		&.right {
+			flex-direction: row-reverse;
+		}
+
+		.text {
+			.title {
+				font-size: 2.3rem;
+				margin-bottom: 2rem;
+			}
+			.description {
+				font-size: 1.2rem;
+				color: var(--color-on-surface);
+			}
+		}
+	}
+
+	@media (max-width: 800px) {
+		.feature {
+			flex-direction: column !important;
+		}
+	}
+</style>
