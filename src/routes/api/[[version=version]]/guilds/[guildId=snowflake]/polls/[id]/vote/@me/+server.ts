e84332755baed2f973b16f0bb09e7f37420993f3
Clembs <clembs@clembs.com>
2023-04-15 03:15:50 +0200

    feat: [wip] poll vote endpoint

---
+++ src/routes/api/[[version=version]]/guilds/[guildId=snowflake]/polls/[id]/vote/@me/+server.ts
@@ -0,0 +1,70 @@
+import { validateAccess } from '$lib/api';
+import { fetchWithCache } from '$lib/cache';
+import { prisma } from '$lib/prisma';
+import { json } from '@sveltejs/kit';
+import type { RequestHandler } from './$types';
+import { formatError } from '$lib/api/genericErrors';
+import type { Poll } from '$lib/api/structures/guild/poll';
+import { z } from 'zod';
+
+export const PUT: RequestHandler = async ({ request, params }) => {
+	let { errorMessage, tokenData } = await validateAccess(
+		request,
+		{ guildId: params.guildId },
+		{ guild: true }
+	);
+	if (errorMessage) return errorMessage;
+
+	params.id = params.id.replace(/_/g, '/');
+
+	const poll = (await fetchWithCache(`poll:${params.id}`, () =>
+		prisma.poll.findFirst({
+			where: { id: params.id }
+		})
+	)) as any as Poll;
+
+	if (!poll) {
+		return formatError('Poll not found.', 404);
+	}
+
+	const body = await request.text();
+
+	try {
+		const newChoiceIndex = z
+			.number()
+			.min(0)
+			.max(poll.choices.length - 1, {
+				message: 'Invalid choice index.'
+			})
+			.parse(parseInt(body));
+
+		const currentVoteIndex = poll.choices.findIndex((c) =>
+			c.participants.find((p) => p === tokenData.userId)
+		);
+
+		if (currentVoteIndex === newChoiceIndex) {
+			return formatError('Can not vote for same choice.', 400);
+		}
+
+		poll.choices[newChoiceIndex].participants.push(tokenData.userId);
+		poll.choices[currentVoteIndex].participants = poll.choices[
+			currentVoteIndex
+		].participants.filter((v) => v !== tokenData.userId);
+
+		const newPoll = await fetchWithCache(
+			`poll:${params.id}`,
+			() =>
+				prisma.poll.update({
+					where: { id: params.id },
+					data: {
+						choices: []
+					}
+				}),
+			true
+		);
+
+		return json(newPoll);
+	} catch (e) {
+		return formatError(e);
+	}
+};
