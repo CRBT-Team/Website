5d6d23beb9aa81c5e919bde333ed84ae13db5d34
Clément Voisin <clembs@clembs.com>
2023-08-13 23:07:51 +0200

    feat!: remove prisma schemas & ts defs

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,17 +1,8 @@
-import type { Rest } from '@purplet/rest';
-import type { APIUser, RESTGetAPICurrentUserGuildsResult } from 'discord-api-types/v10';
-
 declare global {
 	namespace App {
 		interface Error {
 			message: string;
 			stack?: any;
-		}
-
-		export interface Locals {
-			rest: Rest;
-			user: APIUser;
-			guilds: RESTGetAPICurrentUserGuildsResult;
 		}
 	}
 }
